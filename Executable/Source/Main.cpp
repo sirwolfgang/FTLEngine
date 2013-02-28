256589e2eb92c085015b718ec72f29430047cdfb
Zane Wolfgang Pickett <SirWolf@FrostedWolf.com>
2013-02-27 19:24:30 -0500

    Updated Executable to Reflect New Dependencies

--- Executable/Source/Main.cpp
+++ Executable/Source/Main.cpp
@@ -3,14 +3,11 @@
 // Purpose: Entry Point for Game/Engine
 //===============================================================
 #include <FTLCore.h>
-using Core::Engine;
 
 int CALLBACK WinMain(HINSTANCE _hInstance, 
 					 HINSTANCE _hPrevInstance, 
 					 LPSTR _lpCmdLine, 
 					 int _nCmdShow)
 {
-
-	Engine Game(_hInstance, _lpCmdLine, _nCmdShow);
-	return Game.Run();
+	return 0;
 }
